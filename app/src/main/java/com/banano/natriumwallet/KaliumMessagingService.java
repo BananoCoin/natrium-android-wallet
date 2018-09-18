eda498284645353bc633a884ddbd881eb4b774e1
Brandon Berhent <bbedward@gmail.com>
2018-09-18 15:36:32 -0400

    Grouped notifications

--- app/src/main/java/com/banano/natriumwallet/KaliumMessagingService.java
+++ app/src/main/java/com/banano/natriumwallet/KaliumMessagingService.java
@@ -1,16 +1,23 @@
 package com.banano.natriumwallet;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.service.notification.StatusBarNotification;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
 
+import com.banano.natriumwallet.broadcastreceiver.CancelNotificationReceiver;
 import com.banano.natriumwallet.model.Credentials;
 import com.banano.natriumwallet.model.NotificationOption;
 import com.banano.natriumwallet.util.NumberUtil;
@@ -18,7 +25,9 @@ import com.banano.natriumwallet.util.SharedPreferencesUtil;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import io.realm.Realm;
 
@@ -26,11 +35,18 @@ public class KaliumMessagingService extends FirebaseMessagingService {
     private static final String TAG = KaliumMessagingService.class.getSimpleName();
     private static final String NOTIFICATION_CHANNEL_ID = "natrium_notification_channel";
 
+    private int NOTIFICATION_ID = 1337;
+
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         SharedPreferencesUtil sharedPreferencesUtil = new SharedPreferencesUtil(this);
         if (remoteMessage.getData() != null && !MainActivity.appInForeground && sharedPreferencesUtil.getNotificationSetting() != NotificationOption.OFF) {
-            sendNotification(remoteMessage);
+            if (Build.VERSION.SDK_INT >=  Build.VERSION_CODES.M) {
+                sendNotification(remoteMessage);
+            } else {
+                sendNotificationLegacy(remoteMessage);
+            }
         }
     }
 
@@ -54,7 +70,7 @@ public class KaliumMessagingService extends FirebaseMessagingService {
         notificationManager.createNotificationChannel(channel);
     }
 
-    private void sendNotification(RemoteMessage remoteMessage) {
+    private void sendNotificationLegacy(RemoteMessage remoteMessage) {
         Map<String, String> data = remoteMessage.getData();
         String amount = data.get("amount");
         if (amount == null) {
@@ -88,5 +104,92 @@ public class KaliumMessagingService extends FirebaseMessagingService {
         Notification pushNotification = builder.build();
 
         nm.notify((int)System.currentTimeMillis(), pushNotification);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void sendNotification(RemoteMessage remoteMessage) {
+        Map<String, String> data = remoteMessage.getData();
+        String amount = data.get("amount");
+        if (amount == null) {
+            return;
+        }
+
+        initChannels(this);
+
+        try (Realm realm = Realm.getDefaultInstance()) {
+            Credentials c = realm.where(Credentials.class).findFirst();
+            // If not logged in, shouldn't post notifications
+            if (c == null) {
+                return;
+            }
+        }
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        Intent onCancelNotificationReceiver = new Intent(this, CancelNotificationReceiver.class);
+        PendingIntent onCancelNotificationReceiverPendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 0,
+                onCancelNotificationReceiver, 0);
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        StatusBarNotification[] notifications = manager.getActiveNotifications();
+        for (int i = 0; i < notifications.length; i++) {
+            if (notifications[i].getPackageName().equals(getApplicationContext().getPackageName())) {
+                Intent startNotificationActivity = new Intent(this, MainActivity.class);
+                startNotificationActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, startNotificationActivity,
+                        PendingIntent.FLAG_ONE_SHOT);
+                Notification notification = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_status_bar)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                        .setContentTitle(getString(R.string.notification_title, NumberUtil.getRawAsUsableString(amount)))
+                        .setContentText(getString(R.string.notification_body))
+                        .setAutoCancel(true)
+                        .setStyle(getStyleForNotification(getString(R.string.notification_body)))
+                        .setGroupSummary(true)
+                        .setGroup(TAG)
+                        .setContentIntent(pendingIntent)
+                        .setDeleteIntent(onCancelNotificationReceiverPendingIntent)
+                        .build();
+                SharedPreferences sharedPreferences = getSharedPreferences("NotificationData", 0);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(String.valueOf(new Random(NOTIFICATION_ID)), getString(R.string.notification_body));
+                editor.apply();
+                notificationManager.notify(NOTIFICATION_ID, notification);
+                return;
+            }
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        Notification notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_status_bar)
+                .setContentTitle(getString(R.string.notification_title, NumberUtil.getRawAsUsableString(amount)))
+                .setContentText(getString(R.string.notification_body))
+                .setAutoCancel(true)
+                .setGroup(TAG)
+                .setContentIntent(pendingIntent)
+                .setDeleteIntent(onCancelNotificationReceiverPendingIntent)
+                .build();
+        SharedPreferences sharedPreferences = getSharedPreferences("NotificationData", 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(String.valueOf(new Random(NOTIFICATION_ID)), getString(R.string.notification_body));
+        editor.apply();
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder);
+    }
+
+    private NotificationCompat.InboxStyle getStyleForNotification(String messageBody) {
+        NotificationCompat.InboxStyle inbox = new NotificationCompat.InboxStyle();
+        SharedPreferences sharedPref = getSharedPreferences("NotificationData", 0);
+        Map<String, String> notificationMessages = (Map<String, String>) sharedPref.getAll();
+        Map<String, String> myNewHashMap = new HashMap<>();
+        for (Map.Entry<String, String> entry : notificationMessages.entrySet()) {
+            myNewHashMap.put(entry.getKey(), entry.getValue());
+        }
+        inbox.addLine(messageBody);
+        for (Map.Entry<String, String> message : myNewHashMap.entrySet()) {
+            inbox.addLine(message.getValue());
+        }
+        inbox.setBigContentTitle(this.getResources().getString(R.string.app_name))
+                .setSummaryText(getString(R.string.notificaiton_header_suplement));
+        return inbox;
     }
 }
